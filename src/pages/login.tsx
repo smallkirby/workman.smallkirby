223eecd0606d79e41a37729a9c95a48339855ee8
smallkirby <ssmallkirby@gmail.com>
2023-05-17 01:25:57 +0900

    redirect on login success
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,19 @@
 import LoginBox from '@/components/LoginBox';
 import MainLayout from '@/layouts/MainLayout';
+import { FirebaseAuthContext } from '@/lib/firebase/auth';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 
 export default function Login() {
+  const router = useRouter();
+  const authContext = useContext(FirebaseAuthContext);
+
+  useEffect(() => {
+    if (authContext.user) {
+      router.push('/');
+    }
+  }, [authContext.user, router]);
+
   return (
     <MainLayout>
       <div className="mx-auto w-full text-center pt-8">
