64633eab8d3875247042d7d60215936a6596f4a7
smallkirby <ssmallkirby@gmail.com>
2023-05-21 19:28:57 +0900

    set OGP
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import AlertProvider from '@/components/providers/AlertProvider';
 import { ConfigProvider, theme } from 'antd';
 import HistoryProvider from '@/components/providers/HistoryProvider';
 
+const DOMAIN = 'workman.smallkirby.xyz';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
@@ -19,9 +21,32 @@ export default function App({ Component, pageProps }: AppProps) {
             <HistoryProvider>
               <Head>
                 <title>workman</title>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1.0"
+                />
+                <meta
+                  name="description"
+                  content="smallkirby wants to use workman keyboard layout."
+                />
+                <meta property="og:url" content={`https://${DOMAIN}`} />
+                <meta property="og:title" content="workman" />
+                <meta
+                  property="og:site_name"
+                  content="workman.smallkirby.xyz"
+                />
+                <meta
+                  property="og:description"
+                  content="smallkirby wants to use workman keyboard layout."
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                  property="og:image"
+                  content={`https://${DOMAIN}/images/workman-layout.png`}
+                />
                 <link
                   rel="shortcut icon"
-                  href="/favicon.ico?"
+                  href={`https://${DOMAIN}/favicon.ico?`}
                   type="image/x-icon"
                 />
               </Head>
