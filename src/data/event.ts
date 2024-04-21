d31e74dc6304215d3d66e91c12ee444d027aa1aa
smallkirby <ssmallkirby@gmail.com>
2024-04-21 13:00:14 +0900

    event: add `using Workman during daytime`
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/data/event.ts
+++ src/data/event.ts
@@ -3,6 +3,10 @@ import dayjs from 'dayjs';
 
 export const typingEvents: TypingEvent[] = [
   {
+    date: dayjs('2024-04-15T00:00:00.000+09:00').toDate(),
+    description: 'Start using Workman layout during the daytime at work.',
+  },
+  {
     date: dayjs('2024-04-01T00:00:00.000+09:00').toDate(),
     description: 'Start using QWERTY layout during the daytime at work.',
   },
