e89d7e687c095c67dbc3dc3a4fb14329b0181748
smallkirby <ssmallkirby@gmail.com>
2025-03-27 00:06:49 +0900

    test: init E2E test
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

---
+++ tests/global-setup.ts
@@ -0,0 +1,13 @@
+import { spawn } from 'child_process';
+
+let emulatorProcess;
+
+async function globalSetup() {
+  emulatorProcess = spawn('npm', ['run', 'emulate'], { stdio: 'inherit' });
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  (globalThis as typeof globalThis & { __EMULATOR_PROCESS__?: any }).__EMULATOR_PROCESS__ = emulatorProcess;
+}
+
+export default globalSetup;
---
+++ tests/screenshot.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Screenshot of index', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page).toHaveScreenshot('homepage-screenshot.png');
+});
+
+test('Screenshot of admin page', async ({ page }) => {
+  await page.goto('http://localhost:3000/admin');
+  await expect(page).toHaveScreenshot('adminpage-screenshot.png');
+});
