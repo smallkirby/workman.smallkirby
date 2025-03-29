41dfa700ff0f680f68ab7f18f40cde6dad468399
smallkirby <ssmallkirby@gmail.com>
2025-03-29 19:15:56 +0900

    migrate to react v15 and next v19
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  transpilePackages: [
+    'rc-input',
+    'rc-table',
+    'rc-tree',
+    'rc-util',
+    'rc-pagination',
+    'rc-picker',
+    '@ant-design/icons-svg',
+    '@rc-component/util',
+  ],
 };
 
 module.exports = nextConfig;
